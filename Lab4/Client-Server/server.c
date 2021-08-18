c5d6b8f877e02263b60f1baf4b48c198b7bebcda
pratyush-kr <pratyush151198@gmail.com>
2021-08-18 13:31:53 +0530

    Lab4

---
+++ Lab4/Client-Server/server.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<netinet/in.h>
+
+int main()
+{
+    struct sockaddr_in saddr, caddr;
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(1025);
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    printf("Waiting:\n");
+    if(bind(fd, (const struct sockaddr*)&saddr, sizeof(saddr)) < 0)
+    {
+        printf("bind failed\n");
+    }
+    char buffer[25];
+    int len = sizeof(caddr);
+    while(1)
+    {
+        int n = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&caddr, &len);
+        buffer[n] = '\0';
+        printf("Client: %s\n", buffer);
+        if(!strcmp(buffer, "exit"))
+            break;
+        char buff[25];
+        printf("Server: ");
+        scanf(" %[^\n]%*c", buff);
+        sendto(fd, buff, strlen(buff), 0, (const struct sockaddr*)&caddr, len);
+    }
+    close(fd);
+    return 0;
+}
