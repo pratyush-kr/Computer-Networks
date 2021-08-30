cf4a6898336dec0dd1c756dc080825effe74faf6
pratyush-kr <pratyush151198@gmail.com>
2021-08-30 19:29:45 +0530

    Lab4

--- Lab4/Chat-App/1n2/q5Client.c
+++ Lab4/Chat-App/1n2/q5Client.c
@@ -7,21 +7,38 @@
 #include<unistd.h>
 #include<netinet/in.h>
 
+typedef struct Student Student;
+
+struct Student
+{
+    unsigned int roll_number;
+    unsigned int age;
+    char address[255];
+    unsigned int pin_code;
+};
+
 int main()
 {
+    Student *s = (Student*)malloc(sizeof(Student));
     struct sockaddr_in saddr;
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(1025);
     saddr.sin_addr.s_addr = INADDR_ANY;
-    char buff[25];
-    printf("Client: ");
-    scanf(" %[^\n]%*c", buff);
+    int buff;
     int len = sizeof(saddr); 
-    sendto(fd, buff, strlen(buff), 0, (const struct sockaddr*)&saddr, len);
-    int n = recvfrom(fd, buff, sizeof(buff), 0, (struct sockaddr*)&saddr, &len);
-    buff[n] = '\0';
-    printf("Server: %s\n", buff);
+    while(1)
+    {
+        printf("Client: ");
+        scanf(" %d", &buff);
+        sendto(fd, &buff, sizeof(buff), 0, (const struct sockaddr*)&saddr, len);
+        int n = recvfrom(fd, s, sizeof(Student), 0, (struct sockaddr*)&saddr, &len);
+        if(s == NULL) {printf("Student details not found\n"); continue;}
+        printf("roll: %d\n", s->roll_number);
+        printf("age: %d\n", s->age);
+        printf("address: %s\n", s->address);
+        printf("pin: %d\n", s->pin_code);
+    }
     close(fd);
     return 0;
 }
--- Lab4/Chat-App/1n2/q5Server.c
+++ Lab4/Chat-App/1n2/q5Server.c
@@ -20,6 +20,17 @@ struct Student
 int main()
 {
     Student students[5];
+    int rolls[5] = {1, 2, 3, 4, 5};
+    int ages[5] = {21, 21, 22, 21, 20};
+    char addresses[5][255] = {"Ranchi", "Hazaribagh", "Dhanbad", "Bokaro", "Jamshedpur"};
+    int pins[5] = {12345, 12346, 12347, 12348, 12349};
+    for(int i=0; i<5; i++)
+    {
+        students[i].roll_number = rolls[i];
+        students[i].age = ages[i];
+        strcpy(students[i].address, addresses[i]);
+        students[i].pin_code = pins[i];
+    }
     struct sockaddr_in saddr, caddr;
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     saddr.sin_family = AF_INET;
@@ -30,12 +41,24 @@ int main()
     {
         printf("bind failed\n");
     }
-    char buffer[25];
-    int len = sizeof(caddr);
-    int n = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&caddr, &len);
-    buffer[n] = '\0';
-    
-    sendto(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&caddr, len);
+    int buffer;
+    while(1)
+    {
+        int sent = 0;
+        int len = sizeof(caddr);
+        int n = recvfrom(fd, &buffer, sizeof(buffer), 0, (struct sockaddr*)&caddr, &len);
+        printf("%d\n", buffer);
+        for(int i=0; i<5; i++)
+        {
+            if(buffer == students[i].roll_number)
+            {
+                sendto(fd, &students[i], sizeof(Student), 0, (struct sockaddr*)&caddr, len);
+                sent = 1;
+            }
+        }
+        if(!sent)
+            sendto(fd, &students[0], sizeof(Student), 0, (struct sockaddr*)&caddr, len);
+    }
     close(fd);
     return 0;
 }
