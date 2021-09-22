da1f3b2e90a01e444e27fab4bb037e6b34898a6c
pratyush-kr <pratyush151198@gmail.com>
2021-09-22 13:57:34 +0530

    Lab7

--- Lab7/MSC/client.c
+++ Lab7/MSC/client.c
@@ -3,23 +3,43 @@
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
 
-int main()
+void populate(struct sockaddr_in *saddr, int pno)
 {
-    struct sockaddr_in saddr;
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(1024);
-    saddr.sin_addr.s_addr = INADDR_ANY;
-    int len = sizeof(saddr);
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
-    connect(fd, (struct sockaddr*)&saddr, len);
-    printf("Client: ");
-    char buffer[255];
-    scanf(" %c[^\n]", buffer);
-    send(fd, buffer, strlen(buffer), 0);
-    int n = recv(fd, buffer, sizeof(buffer), 0);
-    buffer[n] = '\0';
-    printf("Server: %s\n", buffer);
-    close(fd);
+    saddr->sin_family = AF_INET;
+    saddr->sin_port = pno;
+    saddr->sin_addr.s_addr = INADDR_ANY;
+}
+
+int main(int argc, char *argv[])
+{
+    for(int i=0; i<argc; i++)
+        printf("Pno: %s\n", argv[i]);
+    int n;
+    printf("Servers: ");
+    scanf(" %d", &n);
+    struct sockaddr_in saddr[3];
+    int len = sizeof(saddr[0]);
+    int fd[3] = {0};
+    for(int i=0; i<n; i++)
+        populate(&saddr[i], atoi(argv[1+i]));
+    for(int i=0; i<n; i++)
+        connect(fd[i], (struct sockaddr*)&saddr, len);
+    int i=0;
+    while(i < 3)
+    {
+        printf("Client: ");
+        char buffer[255];
+        scanf(" %[^\n]%*c", buffer);
+        send(fd[i], buffer, strlen(buffer), 0);
+        int n = recv(fd[i], buffer, sizeof(buffer), 0);
+        buffer[n] = '\0';
+        printf("Server %d: %s\n", i+1, buffer);
+        i++;
+    }
+    for(int i=0; i<n; i++)
+        close(fd[i]);
     return 0;
 }
--- Lab7/MSC/server.c
+++ Lab7/MSC/server.c
@@ -3,13 +3,17 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    int pno = atoi(argv[1]);
+    printf("Pno: %d\n", pno);
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in saddr, caddr;
     saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(1024);
+    saddr.sin_port = htons(pno);
     saddr.sin_addr.s_addr = INADDR_ANY;
     int len = sizeof(saddr);
     if(bind(fd, (const struct sockaddr*)&saddr, len) < 0)
@@ -25,7 +29,8 @@ int main()
     buffer[n] = '\0';
     printf("Client: %s\n", buffer);
     printf("Server: ");
-    scanf("%c[^\n]", buffer);
+    scanf("%[^\n]%*c", buffer);
     send(id, buffer, strlen(buffer), 0);
+    close(fd);
     return 0;
 }
